696da1ca39a4c23d548a79b45ca3440769e6dc7e
Brian Yip <itsbriany@gmail.com>
2016-02-22 13:58:12 -0400

    Basic test setup passing

--- RhythmTap/HomeViewController.swift
+++ RhythmTap/HomeViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  HomeViewController.swift
 //  RhythmTap
 //
 //  Created by Brian Yip on 2016-02-22.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class HomeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
---
+++ RhythmTapTests/HomeViewControllerTests.swift
@@ -0,0 +1,34 @@
+//
+//  HomeViewControllerTests.swift
+//  RhythmTap
+//
+//  Created by Brian Yip on 2016-02-22.
+//  Copyright © 2016 Brian Yip. All rights reserved.
+//
+
+import XCTest
+@testable import RhythmTap
+
+class HomeViewControllerTests: XCTestCase {
+    
+    var controller: HomeViewController!
+    
+    override func setUp() {
+        super.setUp()
+        
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let navigationController = storyboard.instantiateViewControllerWithIdentifier("HomeViewNavigationController") as! UINavigationController
+        self.controller = navigationController.topViewController as! HomeViewController
+        
+        // Test and load the view at the same time
+        XCTAssertNotNil(navigationController.view, "HomeViewNavigationController's view is nil")
+        XCTAssertNotNil(self.controller.view, "HomeViewController's view is nil")
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    
+}
